60981b862054a6e6f0ea42df65cb592e2bdac566
uday kumar mailaram <39719416+Chary1729@users.noreply.github.com>
2019-07-09 19:17:26 +0530

    Update ps110.py

--- ps110.py
+++ ps110.py
@@ -1,1 +1,10 @@
+string1,string2 = input().split()
 
+temp = string1
+for i in range(len(string1)):
+    temp = temp.replace(temp[i],string2[i])
+
+if(temp == string2):
+    print("yes")
+else:
+    print("no")
