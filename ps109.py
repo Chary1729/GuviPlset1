8c17930d7bb3d16f250d1fdfceef40b9ac417c7f
uday kumar mailaram <39719416+Chary1729@users.noreply.github.com>
2019-07-09 19:14:56 +0530

    Update ps109.py

--- ps109.py
+++ ps109.py
@@ -1,1 +1,10 @@
-
+lower,upper=map(int,input().split()) 
+count=0  
+for num in range(lower,upper + 1):  
+   if num > 1:  
+       for i in range(2,num):  
+           if (num % i) == 0:  
+               break  
+       else:  
+           count=count+1 
+print(count)
