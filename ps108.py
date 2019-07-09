dd213369f0059b1a81b0d8709d5010be3c2242e4
uday kumar mailaram <39719416+Chary1729@users.noreply.github.com>
2019-07-09 07:03:49 +0530

    Update ps108.py

--- ps108.py
+++ ps108.py
@@ -1,1 +1,11 @@
-
+my_input=input()
+i=1
+print(my_input[0].upper(),end="")
+while i<len(my_input):
+    if ((my_input[i].isspace()) == True):
+        i+=1
+        print(end=" ")
+        print(my_input[i].upper(),end="")
+    else:
+        print(my_input[i],end="")
+    i+=1
